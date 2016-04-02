55e71371c684519ae1488dd89a592635f0896c93
waylay <cristian.ionel@gmail.com>
2016-04-02 17:49:57 +0200

    first commit

---
+++ affiliatewp-hosts-accounts.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: AffiliateWP - Hosts Accounts
+ * Plugin URI: http://webcodesigner.com
+ * Description: Hosts accounts with customers accounts linked on commission basis
+ * Author: Cristian Ionel
+ * Author URI: http://webcodesigner.com
+ * Version: 1.0
+ * Text Domain: affiliatewp-hosts-accounts
+ * Domain Path: languages
+ *
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
